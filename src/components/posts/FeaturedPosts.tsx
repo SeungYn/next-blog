e35ea73033713edbf35a094de9548dd1403d86e9
SeungYn <kwls0407@gmail.com>
2023-04-24 03:44:39 +0900

    refactor: featured posts 타이틀 추가 및 스타일 수정

--- src/components/posts/FeaturedPosts.tsx
+++ src/components/posts/FeaturedPosts.tsx
@@ -3,31 +3,34 @@ import img from 'public/react.png';
 
 export default function FeaturedPosts() {
   return (
-    <section className='grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
-      <PostCard
-        imgSrc={img.src}
-        title='text'
-        date='2023-04-10'
-        content='노드노드노드'
-        category='backend'
-        height={96}
-      />
-      <PostCard
-        imgSrc={img.src}
-        title='text'
-        date='2023-04-10'
-        content='노드노드노드'
-        category='backend'
-        height={96}
-      />
-      <PostCard
-        imgSrc={img.src}
-        title='text'
-        date='2023-04-10'
-        content='노드노드노드'
-        category='backend'
-        height={96}
-      />
+    <section>
+      <h1 className='text-2xl'>Featured Posts</h1>
+      <div className='grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
+        <PostCard
+          imgSrc={img.src}
+          title='text'
+          date='2023-04-10'
+          content='노드노드노드'
+          category='backend'
+          height={96}
+        />
+        <PostCard
+          imgSrc={img.src}
+          title='text'
+          date='2023-04-10'
+          content='노드노드노드'
+          category='backend'
+          height={96}
+        />
+        <PostCard
+          imgSrc={img.src}
+          title='text'
+          date='2023-04-10'
+          content='노드노드노드'
+          category='backend'
+          height={96}
+        />
+      </div>
     </section>
   );
 }
